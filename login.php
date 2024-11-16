193a1f8b15c877d9cf66c86eb522ef4499b541ab
Zer0sky <98915550+Zer0sky2804@users.noreply.github.com>
2024-11-16 18:05:14 +0100

    admin,login

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databasename = "projektV3";
+
+$conn = new mysqli($servername, $username, $password, $databasename);
+
+if ($conn->connect_error) {
+    die("Něco se pokazilo. Prosím zkuste to později: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nickname = $_POST["nickname"];
+    $password = $_POST["password"];
+
+    $nickname = mysqli_real_escape_string($conn, $nickname);
+
+    $sql_check_user = "SELECT * FROM users WHERE nickname = '$nickname'";
+    $result_check_user = $conn->query($sql_check_user);
+
+    if ($result_check_user && $result_check_user->num_rows == 1) {
+        $row = $result_check_user->fetch_assoc();
+
+        if (password_verify($password, $row["password"])) {
+            $_SESSION['user_id'] = $row["user_id"];
+            $_SESSION["user_email"] = $row["email"];
+            $_SESSION["user_nickname"] = $row["nickname"];
+            echo "<script>alert('Úspěšně přihlášen.')</script>";
+            echo "<script>window.location.href = 'admin.php';</script>";
+            exit();
+        } else {
+            echo "<script>alert('Špatné heslo.')</script>";
+            echo "<script>window.location.href = 'index.html';</script>";
+            exit();
+        }
+    } else {
+        echo "<script>alert('Uživatel nenalezen.')</script>";
+        echo "<script>window.location.href = 'index.html';</script>";
+        exit();
+    }
+}
+
+$conn->close();
+?>
---
+++ users.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Sob 16. lis 2024, 17:49
+-- Verze serveru: 10.4.32-MariaDB
+-- Verze PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `projektv3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `nickname` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
